b557775beabb87c79d9172a441a1ce99de6d5b6a
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-03-22 12:01:16 +0100

    Ajoutez des fichiers projet.

---
+++ MonProjetEnCSharpDeTest/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("J'écrit dans la console ! ");
---
+++ net3.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace net3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Je suis en .net core 3.1");
+        }
+    }
+}
